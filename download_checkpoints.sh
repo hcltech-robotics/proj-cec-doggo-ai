2d1cb64c8dd55158d30876de826035df7f7c4ba2
Istvan Fodor <info@istvanfodor.com>
2025-02-20 11:02:49 -0600

    feat: download checkpoints

---
+++ download_checkpoints.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd checkpoints
+wget -P checkpoints https://huggingface.co/hcltech-robotics/gauge_detect/resolve/main/gauge_detect.pt gauge_detect.pt
+wget -P checkpoints https://huggingface.co/hcltech-robotics/gauge_reader/resolve/main/gauge_net_ResidualSEBlock_with_boxes.pt gauge_net_ResidualSEBlock_with_boxes.pt
