a2cbe094e167436810145c9edb77e53a66c77e16
targabor <tar.gabor14@gmail.com>
2025-02-24 21:57:25 +0100

    Add launch file to start gauge_detector and gauge_reader nodes

--- ros.ws/src/gauge_net/gauge_net/setup.py
+++ ros.ws/src/gauge_net/gauge_net/setup.py
@@ -1,27 +1,27 @@
 from setuptools import find_packages, setup
 
-package_name = 'gauge_net'
+package_name = "gauge_net"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", ["launch/gauge_net.launch.py"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='istvan.fodor',
-    maintainer_email='info@istvanfodor.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="istvan.fodor",
+    maintainer_email="info@istvanfodor.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'gauge_detector = gauge_net.gauge_detector:main',
-            'gauge_reader = gauge_net.gauge_reader:main'
-        ],
+        "console_scripts": [
+            "gauge_detector = gauge_net.gauge_detector:main",
+            "gauge_reader = gauge_net.gauge_reader:main",
+        ]
     },
 )
