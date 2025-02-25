ff002b954e73ede2434133c661e1a69b1bdd8d2e
targabor <tar.gabor14@gmail.com>
2025-02-25 13:41:00 +0100

    Fix: Modify formatting to pass colcon test

--- ros.ws/src/gauge_net/gauge_net/gauge_net/gauge_reader.py
+++ ros.ws/src/gauge_net/gauge_net/gauge_net/gauge_reader.py
@@ -1,27 +1,23 @@
-import rclpy
-from rclpy.node import Node
-import torch
-import torchvision.transforms as transforms
-from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from gauge_net_interface.msg import GaugeReading
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+import torch
+import torchvision.transforms as transforms
 
 
 class GaugeReader(Node):
     def __init__(self):
-        super().__init__("resnet_image_processor")
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        super().__init__('resnet_image_processor')
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         # Declare and get model file parameter
-        self.declare_parameter("model_file", "")
-        self.declare_parameter("scaling_min", 0.0)
-        self.declare_parameter("scaling_max", 100.0)
-        model_path = self.get_parameter("model_file").get_parameter_value().string_value
-        self.scaling_min = (
-            self.get_parameter("scaling_min").get_parameter_value().double_value
-        )
-        self.scaling_max = (
-            self.get_parameter("scaling_max").get_parameter_value().double_value
-        )
+        self.declare_parameter('model_file', '')
+        self.declare_parameter('scaling_min', 0.0)
+        self.declare_parameter('scaling_max', 100.0)
+        model_path = self.get_parameter('model_file').get_parameter_value().string_value
+        self.scaling_min = self.get_parameter('scaling_min').get_parameter_value().double_value
+        self.scaling_max = self.get_parameter('scaling_max').get_parameter_value().double_value
 
         # Load ResNet model
         self.model = torch.jit.load(model_path, map_location=self.device)
@@ -33,24 +29,22 @@ class GaugeReader(Node):
                 transforms.ToPILImage(),
                 transforms.Resize((512, 512)),
                 transforms.ToTensor(),
-                transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                ),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
         )
 
         # ROS2 Image subscriber and publisher
         self.image_sub = self.create_subscription(
-            Image, "processed_image", self.image_callback, 10
+            Image, 'processed_image', self.image_callback, 10
         )
-        self.reading_pub = self.create_publisher(GaugeReading, "gauge_reading", 10)
+        self.reading_pub = self.create_publisher(GaugeReading, 'gauge_reading', 10)
 
         self.bridge = CvBridge()
-        self.get_logger().info("ResNet Image Processor Node Started")
+        self.get_logger().info('ResNet Image Processor Node Started')
 
     def image_callback(self, msg):
         # Convert ROS2 image to OpenCV format
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='rgb8')
         # Convert image to tensor
         image_tensor = self.transform(cv_image).unsqueeze(0)  # Add batch dimension
         print(image_tensor)
@@ -59,13 +53,11 @@ class GaugeReader(Node):
         with torch.no_grad():
             reading = self.model(image_tensor).item()
         print(reading)
-        scaled_reading = self.scaling_min + reading * (
-            self.scaling_max - self.scaling_min
-        )
+        scaled_reading = self.scaling_min + reading * (self.scaling_max - self.scaling_min)
         gauge_reading = GaugeReading()
         gauge_reading.reading = reading
         gauge_reading.scaled_reading = scaled_reading
-        print(f"Reading: {reading}, Scaled Reading: {scaled_reading}")
+        print(f'Reading: {reading}, Scaled Reading: {scaled_reading}')
         self.reading_pub.publish(gauge_reading)
 
 
@@ -77,5 +69,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- ros.ws/src/gauge_net/gauge_net/launch/gauge_net.launch.py
+++ ros.ws/src/gauge_net/gauge_net/launch/gauge_net.launch.py
@@ -7,42 +7,38 @@ def generate_launch_description():
     ld = launch.LaunchDescription()
 
     # Declare launch arguments
-    gauge_detector_weights = launch.substitutions.LaunchConfiguration(
-        "gauge_detector_weights"
-    )
-    gauge_reader_weights = launch.substitutions.LaunchConfiguration(
-        "gauge_reader_weights"
-    )
+    gauge_detector_weights = launch.substitutions.LaunchConfiguration('gauge_detector_weights')
+    gauge_reader_weights = launch.substitutions.LaunchConfiguration('gauge_reader_weights')
 
     ld.add_action(
         launch.actions.DeclareLaunchArgument(
-            "gauge_detector_weights", description="Path to weights for gauge_detector"
+            'gauge_detector_weights', description='Path to weights for gauge_detector'
         )
     )
 
     ld.add_action(
         launch.actions.DeclareLaunchArgument(
-            "gauge_reader_weights", description="Path to weights for gauge_reader"
+            'gauge_reader_weights', description='Path to weights for gauge_reader'
         )
     )
 
     # Add gauge_detector node
     ld.add_action(
         launch_ros.actions.Node(
-            package="gauge_net",
-            executable="gauge_detector",
-            name="gauge_detector",
-            parameters=[{"model_file": gauge_detector_weights}],
+            package='gauge_net',
+            executable='gauge_detector',
+            name='gauge_detector',
+            parameters=[{'model_file': gauge_detector_weights}],
         )
     )
 
     # Add gauge_reader node
     ld.add_action(
         launch_ros.actions.Node(
-            package="gauge_net",
-            executable="gauge_reader",
-            name="gauge_reader",
-            parameters=[{"model_file": gauge_reader_weights}],
+            package='gauge_net',
+            executable='gauge_reader',
+            name='gauge_reader',
+            parameters=[{'model_file': gauge_reader_weights}],
         )
     )
 
