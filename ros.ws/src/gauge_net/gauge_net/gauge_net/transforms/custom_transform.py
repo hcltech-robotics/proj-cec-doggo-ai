feat: merge two gauge nodes, add namespace, fix custom transform needle box scaling, add image topic to launch file

ros.ws/src/gauge_net/gauge_net/gauge_net/transforms/custom_transform.py
@@ -1,9 +1,10 @@
-import numpy as np
 import cv2
+import numpy as np
 from PIL import Image, ImageOps
 
 
 class CLAHEPreprocess:
+
     def __init__(self, clip_limit=5.0, tile_grid_size=(10, 10), min_area=15000):
         self.clip_limit = clip_limit
         self.tile_grid_size = tile_grid_size
@@ -51,6 +52,7 @@ class CLAHEPreprocess:
 
 
 class ResizeWithPaddingAndBBox:
+
     def __init__(self, target_size=(512, 512), padding_color=0):
         self.target_size = target_size
         self.padding_color = padding_color
@@ -67,33 +69,38 @@ class ResizeWithPaddingAndBBox:
         scale_x = new_width / original_width
         scale_y = new_height / original_height
 
+        # Scale the bounding box from normalized to pixel coordinates
         orig_bbox = bbox * np.array(
             [original_width, original_height, original_width, original_height]
         )
         orig_bbox = orig_bbox.astype(int)
 
         # Scale the bounding box
-        bbox = [
+        scaled_bbox_pixels = [
             orig_bbox[0] * scale_x,
             orig_bbox[1] * scale_y,
             orig_bbox[2] * scale_x,
             orig_bbox[3] * scale_y,
         ]
-        # Calculate padding to center the image
+
+        # Calculate padding to fill target size (right and bottom only)
         width_padding = max(0, (self.target_size[0] - new_width))
         height_padding = max(0, (self.target_size[1] - new_height))
 
-        # Apply padding
+        # Apply padding to right and bottom only
         img = ImageOps.expand(
             img, border=(0, 0, width_padding, height_padding), fill=self.padding_color
         )
 
-        # Adjust the bounding box based on the scaling
+        # Get final image dimensions (after padding)
+        final_width, final_height = img.size
+
+        # Normalize the bounding box based on the final padded image dimensions
         scaled_bbox = [
-            bbox[0] / new_width,
-            bbox[1] / new_height,
-            bbox[2] / new_width,
-            bbox[3] / new_height,
-        ]  # xmin  # ymin  # xmax  # ymax
+            scaled_bbox_pixels[0] / final_width,
+            scaled_bbox_pixels[1] / final_height,
+            scaled_bbox_pixels[2] / final_width,
+            scaled_bbox_pixels[3] / final_height,
+        ]
 
         return {'image': img, 'bbox': scaled_bbox}